c388b2bce3799d63e792040c5ce4ce6e203ce47e
Ridwan Farouki <ridwan@simprints.com>
2018-07-15 19:12:03 +0100

    Convert to Kotlin

---
+++ desktop/src/com/ridwanstandingby/ntris/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.ridwanstandingby.ntris.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.ridwanstandingby.ntris.NTrisGame
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(NTrisGame(), config)
+    }
+}
