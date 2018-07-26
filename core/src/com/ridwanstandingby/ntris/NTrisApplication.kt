39d7baee40f1f2d0cd4af224470dd0812c5099b4
Ridwan Farouki <ridwan@simprints.com>
2018-07-26 01:22:16 +0100

    removed unused imports

--- core/src/com/ridwanstandingby/ntris/NTrisApplication.kt
+++ core/src/com/ridwanstandingby/ntris/NTrisApplication.kt
@@ -5,8 +5,6 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
-import com.ridwanstandingby.ntris.render.Dimensions
-import com.ridwanstandingby.ntris.render.fonts.FontGenerator
 import com.ridwanstandingby.ntris.states.GameStateManager
 import com.ridwanstandingby.ntris.states.PlayState
 
