b13779cf517b8f7d973575e1ceeb8636e59f98cb
Ridwan Farouki <ridwan@simprints.com>
2018-08-19 22:59:26 +0100

    Promote inputprocessor to State.kt

--- core/src/com/ridwanstandingby/ntris/states/PlayState.kt
+++ core/src/com/ridwanstandingby/ntris/states/PlayState.kt
@@ -1,7 +1,5 @@
 package com.ridwanstandingby.ntris.states
 
-import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.InputAdapter
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.ridwanstandingby.ntris.input.RawPlayInputProcessor
@@ -13,10 +11,6 @@ class PlayState(gsm: GameStateManager) : State(gsm) {
 
     private var views: List<View> = LayoutArranger(gsm.dimensions, gsm.fonts).createViews()
     private val rawInputProcessor = RawPlayInputProcessor(gsm)
-
-    init {
-        configureInputProcessor()
-    }
 
     override fun handleInput(): Boolean {
         gsm.game.resolvePlayInput(rawInputProcessor.getRawInput(views))
@@ -42,21 +36,11 @@ class PlayState(gsm: GameStateManager) : State(gsm) {
     }
 
     private fun endRender(sb: SpriteBatch, sr: ShapeRenderer) {
-        // Bug workaround - need to renderPolyomino a new font for the previous ones to renderPolyomino
+        // Bug workaround - need to render a new font for the previous ones to render
         gsm.fonts.dummy.draw(sb, " ", 0f, 0f)
         sr.end()
         sb.end()
     }
 
     override fun dispose() {}
-
-    private fun configureInputProcessor() {
-        Gdx.input.inputProcessor = object : InputAdapter() {
-            override fun keyDown(keycode: Int) = handleInput()
-            override fun touchUp(screenX: Int, screenY: Int, pointer: Int, button: Int) = handleInput()
-            override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int) = handleInput()
-            override fun touchDragged(screenX: Int, screenY: Int, pointer: Int) = handleInput()
-            override fun keyUp(keycode: Int) = handleInput()
-        }
-    }
 }
--- core/src/com/ridwanstandingby/ntris/states/State.kt
+++ core/src/com/ridwanstandingby/ntris/states/State.kt
@@ -1,5 +1,7 @@
 package com.ridwanstandingby.ntris.states
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputAdapter
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
@@ -10,10 +12,21 @@ abstract class State(protected var gsm: GameStateManager) {
 
     init {
         cam.setToOrtho(false, gsm.dimensions.screenWidth.toFloat(), gsm.dimensions.screenHeight.toFloat())
+        configureInputProcessor()
     }
 
     abstract fun handleInput(): Boolean
     abstract fun update(dt: Float)
     abstract fun render(sb: SpriteBatch, sr: ShapeRenderer)
     abstract fun dispose()
+
+    private fun configureInputProcessor() {
+        Gdx.input.inputProcessor = object : InputAdapter() {
+            override fun keyDown(keycode: Int) = handleInput()
+            override fun touchUp(screenX: Int, screenY: Int, pointer: Int, button: Int) = handleInput()
+            override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int) = handleInput()
+            override fun touchDragged(screenX: Int, screenY: Int, pointer: Int) = handleInput()
+            override fun keyUp(keycode: Int) = handleInput()
+        }
+    }
 }
