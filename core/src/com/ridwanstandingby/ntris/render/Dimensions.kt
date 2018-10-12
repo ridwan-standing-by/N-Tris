da10ad1483da6931dc115b1ad95342e8ff703faa
Ridwan Farouki <ridwan@simprints.com>
2018-10-12 14:48:15 +0100

    Working centring with extra screen size

--- core/src/com/ridwanstandingby/ntris/render/Dimensions.kt
+++ core/src/com/ridwanstandingby/ntris/render/Dimensions.kt
@@ -11,12 +11,22 @@ class Dimensions(val screenWidth: Int, val screenHeight: Int) {
 
     val block = 60f
 
+    val extraBlocksX: Float
+    val extraBlocksY: Float
 
     init {
         val scaleX = screenWidth / gameWidth.toFloat()
         val scaleY = screenHeight / gameHeight.toFloat()
 
         scale = min(scaleX, scaleY)
+
+        extraBlocksX = calculateExtraBlocks(screenWidth, gameWidth)
+        extraBlocksY = calculateExtraBlocks(screenHeight, gameHeight)
+    }
+
+    private fun calculateExtraBlocks(screenPixels: Int, gameDim: Int): Float {
+        val gamePixels = screenPixels.toFloat() - rescale(gameDim)
+        return gamePixels / rescaledBlock()
     }
 
     fun rescale(x: Int): Float = (scale * x.toFloat())
--- core/src/com/ridwanstandingby/ntris/render/views/LayoutArranger.kt
+++ core/src/com/ridwanstandingby/ntris/render/views/LayoutArranger.kt
@@ -14,28 +14,28 @@ class LayoutArranger(private val dimensions: Dimensions, private val fonts: Font
 
     fun createViews(): ArrayList<View> = arrayListOf<View>().apply {
         add(MoveDownButtonView(dimensions, fonts,
-                10.5f, 01.0f, 4.0f, 4.0f))
+                dimensions.extraBlocksX/2f + 10.5f, dimensions.extraBlocksY/2f + 01.0f, 4.0f, 4.0f))
         add(ReflectButtonView(dimensions, fonts,
-                14.5f, 01.0f, 4.0f, 4.0f))
+                dimensions.extraBlocksX/2f + 14.5f, dimensions.extraBlocksY/2f + 01.0f, 4.0f, 4.0f))
         add(MoveLeftButtonView(dimensions, fonts,
-                10.5f, 05.0f, 4.0f, 4.0f))
+                dimensions.extraBlocksX/2f + 10.5f, dimensions.extraBlocksY/2f + 05.0f, 4.0f, 4.0f))
         add(MoveRightButtonView(dimensions, fonts,
-                14.5f, 05.0f, 4.0f, 4.0f))
+                dimensions.extraBlocksX/2f + 14.5f, dimensions.extraBlocksY/2f + 05.0f, 4.0f, 4.0f))
         add(RotateLeftButtonView(dimensions, fonts,
-                10.5f, 09.0f, 4.0f, 4.0f))
+                dimensions.extraBlocksX/2f + 10.5f, dimensions.extraBlocksY/2f + 09.0f, 4.0f, 4.0f))
         add(RotateRightButtonView(dimensions, fonts,
-                14.5f, 09.0f, 4.0f, 4.0f))
+                dimensions.extraBlocksX/2f + 14.5f, dimensions.extraBlocksY/2f + 09.0f, 4.0f, 4.0f))
         add(ReserveView(dimensions, fonts,
-                10.5f, 13.0f, 8.0f, 7.0f))
+                dimensions.extraBlocksX/2f + 10.5f, dimensions.extraBlocksY/2f + 13.0f, 8.0f, 7.0f))
         add(ScoreView(dimensions, fonts,
-                10.5f, 20.0f, 8.0f, 4.0f))
+                dimensions.extraBlocksX/2f + 10.5f, dimensions.extraBlocksY/2f + 20.0f, 8.0f, 4.0f))
         add(NextView(dimensions, fonts,
-                10.5f, 24.0f, 8.0f, 7.0f))
+                dimensions.extraBlocksX/2f + 10.5f, dimensions.extraBlocksY/2f + 24.0f, 8.0f, 7.0f))
         add(PlayView(dimensions, fonts,
-                00.5f, 01.0f,
+                dimensions.extraBlocksX/2f + 00.5f, dimensions.extraBlocksY/2f + 01.0f,
                 GameRules.PLAY_BLOCK_SIZE.x.toFloat(),
                 GameRules.PLAY_BLOCK_SIZE.y.toFloat()))
         add(GameOverView(dimensions, fonts,
-                01.5f, 13.0f, 8.0f, 7.0f))
+                dimensions.extraBlocksX/2f + 01.5f, dimensions.extraBlocksY/2f + 13.0f, 8.0f, 7.0f))
     }
 }
