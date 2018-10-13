436fecc9e210167edca6bbac1aec3bc851e53752
Ridwan Farouki <ridwan@simprints.com>
2018-10-13 02:51:47 +0100

    Made game slightly more difficult again

--- core/src/com/ridwanstandingby/ntris/game/GameRules.kt
+++ core/src/com/ridwanstandingby/ntris/game/GameRules.kt
@@ -5,7 +5,7 @@ import com.ridwanstandingby.ntris.polyomino.geometry.IntVector2
 object GameRules {
 
     val PLAY_BLOCK_SIZE = IntVector2(10, 30)
-    const val PULSE_TIME = 0.9f
+    const val PULSE_TIME = 0.85f
     const val GENEROUS_MANIPULATION_MAX_MOVES = 2
     val linesToScore = { lines: Int -> 5 * lines * (lines + 1) }
 }
