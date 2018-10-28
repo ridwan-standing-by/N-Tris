7b3b0a18292ae6c1eaf6eea6c2db140dc00302c5
Ridwan Farouki <ridwan@simprints.com>
2018-10-28 11:39:05 +0000

    Configure canonical build types and add debug services.json

--- android/src/main/kotlin/com/ridwanstandingby/ntris/activities/leaderboard/LeaderboardViewModel.kt
+++ android/src/main/kotlin/com/ridwanstandingby/ntris/activities/leaderboard/LeaderboardViewModel.kt
@@ -3,6 +3,7 @@ package com.ridwanstandingby.ntris.activities.leaderboard
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
 import com.ridwanstandingby.ntris.domain.ScoreEntry
 
 class LeaderboardViewModel : ViewModel() {
@@ -10,9 +11,9 @@ class LeaderboardViewModel : ViewModel() {
     var scoreEntries: MutableLiveData<List<ScoreEntry>> = MutableLiveData()
 
     fun getScoreEntries(): LiveData<List<ScoreEntry>> {
-        if (scoreEntries.value == null) {
-            FirebaseDatabase.getInstance
-        }
+//        if (scoreEntries.value == null) {
+//            FirebaseFirestore.getInstance().collection()
+//        }
         return scoreEntries
     }
 }
