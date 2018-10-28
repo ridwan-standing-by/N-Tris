4996a06e992629cf385504230ff67b5e57c8d45c
Ridwan Farouki <ridwan@simprints.com>
2018-10-28 20:23:08 +0000

    Improved ViewModel usage

--- android/src/main/kotlin/com/ridwanstandingby/ntris/activities/leaderboard/LeaderboardActivity.kt
+++ android/src/main/kotlin/com/ridwanstandingby/ntris/activities/leaderboard/LeaderboardActivity.kt
@@ -1,7 +1,7 @@
 package com.ridwanstandingby.ntris.activities.leaderboard
 
 import android.arch.lifecycle.Observer
-import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
@@ -13,18 +13,25 @@ class LeaderboardActivity : AppCompatActivity() {
 
     private val remoteDbManager by lazy { (application as Application).remoteDataManager }
 
-    private val leaderboardViewModel by lazy {
-        ViewModelProvider.AndroidViewModelFactory.getInstance(application)
-                .create(LeaderboardViewModel::class.java)
-    }
-
-    private val scoreEntryAdapter by lazy { ScoreEntryAdapter(this) }
+    private lateinit var scoreEntryAdapter: ScoreEntryAdapter
+    private lateinit var leaderboardViewModel: LeaderboardViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_leaderboard)
+
+        initRecyclerView()
+        initLeaderboardViewModel()
+    }
+
+    private fun initRecyclerView() {
+        scoreEntryAdapter = ScoreEntryAdapter(this)
         leaderboardRecyclerView.layoutManager = LinearLayoutManager(this)
         leaderboardRecyclerView.adapter = scoreEntryAdapter
+    }
+
+    private fun initLeaderboardViewModel() {
+        leaderboardViewModel = ViewModelProviders.of(this).get(LeaderboardViewModel::class.java)
         leaderboardViewModel.getScoreEntries(remoteDbManager).observe(this, Observer { list ->
             if (list != null) {
                 scoreEntryAdapter.items = list.sortedByDescending { it.score }
--- android/src/main/kotlin/com/ridwanstandingby/ntris/activities/leaderboard/LeaderboardViewModel.kt
+++ android/src/main/kotlin/com/ridwanstandingby/ntris/activities/leaderboard/LeaderboardViewModel.kt
@@ -3,20 +3,23 @@ package com.ridwanstandingby.ntris.activities.leaderboard
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
-import com.google.firebase.firestore.FirebaseFirestore
 import com.ridwanstandingby.ntris.data.remote.RemoteDataManager
 import com.ridwanstandingby.ntris.domain.ScoreEntry
 
 class LeaderboardViewModel : ViewModel() {
 
-    val scoreEntries: MutableLiveData<List<ScoreEntry>> = MutableLiveData()
+    private val scoreEntries: MutableLiveData<List<ScoreEntry>> = MutableLiveData()
 
     fun getScoreEntries(remoteDataManager: RemoteDataManager): LiveData<List<ScoreEntry>> {
         if (scoreEntries.value == null) {
-            remoteDataManager.downloadScoreEntries(
-                    onSuccess = { scoreEntries.postValue(it) },
-                    onError = { it.printStackTrace() })
+            loadScoreEntries(remoteDataManager)
         }
         return scoreEntries
+    }
+
+    private fun loadScoreEntries(remoteDataManager: RemoteDataManager) {
+        remoteDataManager.downloadScoreEntries(
+                onSuccess = { scoreEntries.postValue(it) },
+                onError = { it.printStackTrace() })
     }
 }
