0e3b8d64761db0798090ac4de92e9617fbb0a49a
Ridwan Farouki <ridwan@simprints.com>
2018-10-29 00:19:20 +0000

    Added linkage of nickname

--- android/src/main/kotlin/com/ridwanstandingby/ntris/activities/MainActivity.kt
+++ android/src/main/kotlin/com/ridwanstandingby/ntris/activities/MainActivity.kt
@@ -4,11 +4,14 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import com.ridwanstandingby.ntris.Application
 import com.ridwanstandingby.ntris.R
 import com.ridwanstandingby.ntris.activities.leaderboard.LeaderboardActivity
 import com.ridwanstandingby.ntris.data.GameDataManager
+import com.ridwanstandingby.ntris.data.SharedPreferencesManager
 import com.ridwanstandingby.ntris.render.views.LayoutArrangement
 import kotlinx.android.synthetic.main.layout_main.*
 import org.jetbrains.anko.doAsync
@@ -17,10 +20,12 @@ import org.jetbrains.anko.uiThread
 class MainActivity : AppCompatActivity() {
 
     private lateinit var gameDataManager: GameDataManager
+    private lateinit var sharedPreferencesManager: SharedPreferencesManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_main)
+        sharedPreferencesManager = (application as Application).sharedPreferencesManager
         gameDataManager = (application as Application).gameDataManager
 
         if (gameDataManager.polyominoBlueprintHolder != null) {
@@ -34,6 +39,7 @@ class MainActivity : AppCompatActivity() {
         leaderboardButton.setOnClickListener { launchLeaderboardActivity() }
         howToPlayButton.setOnClickListener { launchTutorialActivity() }
     }
+
     private fun beginLoading() {
         playButton.visibility = View.INVISIBLE
         loadingProgressBar.visibility = View.VISIBLE
@@ -57,19 +63,26 @@ class MainActivity : AppCompatActivity() {
     private fun launchTutorialActivity() = startActivity<TutorialActivity>()
     private fun launchGameActivity() = startActivity<GameActivity>()
 
-    private inline fun <reified T: Activity> startActivity() {
+    private inline fun <reified T : Activity> startActivity() {
         val intent = Intent(this, T::class.java)
         startActivity(intent)
     }
 
     private fun initNicknameEditText() {
-
+        nicknameEditText.setText(sharedPreferencesManager.nickname)
+        nicknameEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                sharedPreferencesManager.nickname = s.toString()
+            }
+        })
     }
 
     private fun initHandednessSwitch() {
-        handednessSwitch.setCheckedImmediately(gameDataManager.layoutArrangement.toBoolean())
+        handednessSwitch.setCheckedImmediately(sharedPreferencesManager.layoutArrangement.toBoolean())
         handednessSwitch.setOnCheckedChangeListener { _, isChecked ->
-            gameDataManager.layoutArrangement = isChecked.toLayoutArrangement()
+            sharedPreferencesManager.layoutArrangement = isChecked.toLayoutArrangement()
         }
     }
 
